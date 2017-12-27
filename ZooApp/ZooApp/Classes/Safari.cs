6218ca8cff0679a2707b40d1259a54b3c4f3c633
Luay <luayghazwan@yahoo.com>
2017-12-27 00:20:36 -0800

    Safari class derived from Zoo base class

---
+++ ZooApp/ZooApp/Classes/Safari.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApp.Classes
+{
+    abstract class Safari : Zoo
+    {
+        public string Theme { get; set; }
+
+        public bool Desert { get; set; }
+
+        public int DangerLevel { get; set; }
+
+        public void WatchAnimals()
+        {
+            Console.WriteLine("Safaris are awesome beause we get to see wild animals!");
+        }
+
+        public TimeSpan TimeVisited()
+        {
+            return DateTime.Now.TimeOfDay;
+        }
+
+        public bool LearnBehavior()
+        {
+            return true;
+        }
+    }
+}
