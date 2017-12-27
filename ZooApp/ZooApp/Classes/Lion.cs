308fc0ae98d1d7df7a6ffa51a7e56e2f5653dca9
Luay <luayghazwan@yahoo.com>
2017-12-27 00:56:20 -0800

    Lion class created

---
+++ ZooApp/ZooApp/Classes/Lion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApp.Classes
+{
+    class Lion : AfricanLion
+    {
+        public int AngerLevel { get; set; }
+        public bool IsKing { get; set; }
+        public bool HasChildren { get; set; }
+
+        public void LookForAPrey()
+        {
+            Console.WriteLine("Finding my food is my easiest job ;)");
+        }
+
+        public void Roar()
+        {
+            Console.WriteLine("I am the king and I scare all animals! Though, I have a very sweet heart.");
+        }
+    }
+}
