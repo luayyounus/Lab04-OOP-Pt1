7ae0d70679e2e2d0439c24660fd916330d802e5a
Luay <luayghazwan@yahoo.com>
2017-12-27 00:47:52 -0800

    Zebra class derived from African Lion Safari

---
+++ ZooApp/ZooApp/Classes/Zebra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApp.Classes
+{
+    class Zebra : AfricanLion
+    {
+        public string SpeciesName { get; set; }
+        public bool Stripes { get; set; }
+        public float Weight { get; set; }
+
+        public void KeepCool()
+        {
+            Console.WriteLine("Isn't that what Zebras do all the time?");
+        }
+
+        public int CheckMinimumDangerLevel()
+        {
+            return DangerLevel;
+        }
+    }
+}
