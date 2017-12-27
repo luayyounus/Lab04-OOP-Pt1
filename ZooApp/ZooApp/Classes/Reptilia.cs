5f7aac66017d70bcb01c92d29bc0fe080a8f7ef1
Luay <luayghazwan@yahoo.com>
2017-12-27 01:10:31 -0800

    Reptilia class derived from Niagara

---
+++ ZooApp/ZooApp/Classes/Reptilia.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApp.Classes
+{
+    class Reptilia : Niagara
+    {
+        public bool AnimalExistance { get; set; }
+        public int NumberOfGame { get; set; }
+
+        public void DoActivity()
+        {
+            base.TakeARide();
+        }
+
+        public bool IsAThing()
+        {
+            Console.WriteLine("I don't know why Reptilia is a thing!");
+            return false;
+        }
+    }
+}
