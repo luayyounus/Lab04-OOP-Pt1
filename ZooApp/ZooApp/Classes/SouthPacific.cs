6dd9ca01115658debf645f31a74e3388a902929b
Luay <luayghazwan@yahoo.com>
2017-12-27 01:17:45 -0800

    South pacific class derived from Base Aquaria

---
+++ ZooApp/ZooApp/Classes/SouthPacific.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZooApp.Classes
+{
+    class SouthPacific : Aquaria
+    {
+        public int Speed { get; set; }
+        public string[] Colors { get; set; }
+        public float Length { get; set; }
+
+        public void MatchWithNemo(int speed)
+        {
+            if (speed == this.Speed)
+            {
+                Console.WriteLine("Match!!!");
+            }
+        }
+
+        public string NumberOfRareAnimal(string fish)
+        {
+            return this.Colors[base.NumberOfRareAnimals];
+        }
+    }
+}
