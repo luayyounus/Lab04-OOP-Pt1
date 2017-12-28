4461c02309a858f9936645b6ff7d40ee5ea28915
Luay <luayghazwan@yahoo.com>
2017-12-28 11:59:47 -0800

    Method for underlying property

--- ZooApp/ZooApp/Classes/AfricanLion.cs
+++ ZooApp/ZooApp/Classes/AfricanLion.cs
@@ -6,16 +6,21 @@ namespace ZooApp.Classes
 {
     public class AfricanLion : Safari
     {
-        private TimeSpan visitTime;
+        private TimeSpan _visitTime;
         public TimeSpan VisitTime
         {
             get { return base.TimeVisited(); }
-            set { this.visitTime = value; }
+            set { this._visitTime = value; }
         }
 
         public void LearnAboutLionsWhenResting()
         {
             Console.WriteLine("Calm and fearless are two of the Lion's qualities.");
+        }
+
+        public string TestingOutUnderlyingProperty()
+        {
+            return "Visit time is: " + _visitTime;
         }
 
         public bool IsFriendsWithRhino()
